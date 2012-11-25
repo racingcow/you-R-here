a24aaadce72fcdc3fbb3f35e98588dbe80fdec53
David Miller <racingcow@hotmail.com>
2012-11-24 20:30:19 -0600

    Moved views into vies.js

--- you-R-here.js
+++ you-R-here.js
@@ -24,26 +24,27 @@ var _users = []; //users currently connected
 var _demoItems = []; //user stories and bugs 
 var _activeItemId = 0;
 var _staticContentItems = {
-    title: 'you-R-here',
+    title: "you-R-here",
     scripts: [
-        '/socket.io/socket.io.js',
-        'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js',
-        'https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js',
-        'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js',
-        //'http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone.js',
-        'client_libs/backbone.js', //development version
-        'http://cdn.jsdelivr.net/jgrowl/1.2.6/jquery.jgrowl_minimized.js',
-        'client_libs/backbone.iobind.js',
-        'client_libs/backbone.iosync.js',
-        'client_libs/moment.min.js',
-        'client_script/model.js'
+        "/socket.io/socket.io.js",
+        "https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js",
+        "https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js",
+        "http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.2/underscore-min.js",
+        //"http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone.js",
+        "client_libs/backbone.js", //development version
+        "http://cdn.jsdelivr.net/jgrowl/1.2.6/jquery.jgrowl_minimized.js",
+        "client_libs/backbone.iobind.js",
+        "client_libs/backbone.iosync.js",
+        "client_libs/moment.min.js",
+        "client_script/model.js",
+        "client_script/views.js"
     ],
     styles: [
-        'css/index.css',
-        'css/jquery.jgrowl.css',
-        'css/jquery-ui-1.8.21.custom.css'
+        "css/index.css",
+        "css/jquery.jgrowl.css",
+        "css/jquery-ui-1.8.21.custom.css"
     ],
-    version: JSON.parse(_fs.readFileSync('package.json', 'utf8')).version, //get the version from the package.json file and hand it off to the views
+    version: JSON.parse(_fs.readFileSync("package.json", "utf8")).version, //get the version from the package.json file and hand it off to the views
     address: _address,
     port: _port
 };
@@ -57,15 +58,15 @@ _tp.api("getUserImage", function(data) {
 res.send(image, "binary");
 }, { id: req.params["id"] }); }); */ 
 _app.get("/", function(req, res) {         
-    res.render('spectator.jade', _staticContentItems);
+    res.render("spectator.jade", _staticContentItems);
 });
-_app.get('/admin.html', function (req, res) {
-    res.render('organizer.jade', _staticContentItems);
+_app.get("/admin.html", function (req, res) {
+    res.render("organizer.jade", _staticContentItems);
 });
-_app.get('/presenter.html', function (req, res) {
-    res.render('presenter.jade', _staticContentItems);
+_app.get("/presenter.html", function (req, res) {
+    res.render("presenter.jade", _staticContentItems);
 });
-_app.get('/*', function(req, res){         
+_app.get("/*", function(req, res){         
     var uri = _url.parse(req.url).pathname;         
     var filename = _path.join(process.cwd(), uri);         
     _path.exists(filename, function(exists){
@@ -76,7 +77,7 @@ _app.get('/*', function(req, res){
             return;
         } 
         _fs.readFile(filename, "binary", function(err, file){                
-            res.writeHead(200, {'Content-Type' : _mime.lookup(filename) });        
+            res.writeHead(200, {"Content-Type" : _mime.lookup(filename) });        
             res.write(file, "binary");        
             res.end();
         });          
@@ -131,7 +132,7 @@ _io.sockets.on("connection", function (socket) {
         if (!newUser.email || _.trim(newUser.email) === "") return;
 
         newUser.id = _uuid.v4(); //backbone.iobind loses its mind if ids are not unique
-        newUser.gravatarUrl = _gravatar.url(newUser.email, { size: '80', default: 'identicon' });
+        newUser.gravatarUrl = _gravatar.url(newUser.email, { size: "80", default: "identicon" });
         _users.push(newUser);
 
         socket.userid = newUser.id;
@@ -188,7 +189,7 @@ _io.sockets.on("connection", function (socket) {
     //socket.on("adduser", function(username) { 
         
     //    //get the gravatar for this user
-    //    var gravatarUrl = _gravatar.url(username, { size: '80', default:'identicon'}); 
+    //    var gravatarUrl = _gravatar.url(username, { size: "80", default:"identicon"}); 
         
     //    //store username in the socket session for this client, and in global list
     //    socket.username = username;
@@ -285,7 +286,7 @@ function tpToModelSchema(data, boundaryDate) {
             description: item.Description,
             project: item.Project.Name,
             type: item.EntityType.Name,
-            demonstratorName: assignedUser.FirstName + ' ' + assignedUser.LastName,
+            demonstratorName: assignedUser.FirstName + " " + assignedUser.LastName,
             demonstratorEmail: assignedUser.Email,
             demonstrable: true,
             demonstrated: false,
