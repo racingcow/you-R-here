940eb1c06708a46505812368916d358f7d6acce6
Rick Yoesting <rick.yoesting@gmail.com>
2013-04-19 14:33:12 -0500

    remove excess stuff

--- client_script/admin.js
+++ client_script/admin.js
@@ -7,35 +7,6 @@ YouRHere.App = Backbone.Router.extend({
         '/': 'index'
     },
     index: function () {
-        /*
-        var users = new YouRHere.Users(),
-            userListView = new YouRHere.UserListView(users, 'admin');
-
-        $("#users").append(userListView.el);
-        users.fetch();
-
-        var iteration = new YouRHere.Iteration(),
-            iterationView = new YouRHere.IterationView(iteration);
-        iteration.fetch();
-
-        var demoItems = new YouRHere.DemoItems(),
-            demoListView = new YouRHere.DemoListView(YouRHere.EditableDemoItemView, demoItems, { 
-                sortable: true,
-            });
-
-        $("#itemsView").hide().append(demoListView.el);
-        demoItems.fetch();
-
-
-        //Capture the email address of the currently logged in user from the users view
-        userListView.on("user:login", function (email) {
-            YouRHere.Utils.log("Router received 'user:login' event - email is " + email);
-            demoListView.email = email;
-            //always click the first item!
-            //$('#DemoListView').first().click();
-            $("#itemsView").show();
-        });*/
-
         $("#users").hide();
         $("#itemsView").show();
     }
@@ -44,8 +15,4 @@ YouRHere.App = Backbone.Router.extend({
 $(document).ready(function () {
     var app = new YouRHere.App();
     Backbone.history.start();
-
-    //DCM - replaced with iteration view
-    //$('#datepicker').datepicker({dateFormat: 'mm-dd-yy'});
-
 });
