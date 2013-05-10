e1f7fab07f957d46d777e89553b94d9b58c2c145
Rick Yoesting <rick.yoesting@gmail.com>
2013-05-10 00:11:00 -0500

    I wanted a way to output the human readable request and uri-encoded request

--- targetprocess.js
+++ targetprocess.js
@@ -1,34 +1,36 @@
-var sys = require("util");
-var rest = require("restler"); //https://github.com/danwrong/restler 
-var moment = require("moment");//http://momentjs.com
-var config = require("./targetprocess.config");
-var _ = require("underscore");
+var sys = require('util');
+var rest = require('restler'); //https://github.com/danwrong/restler 
+var moment = require('moment');//http://momentjs.com
+var config = require('./targetprocess.config');
+var _ = require('underscore');
 
 var self = this;
 var methods = {    
     globalOptions : {},
     init : function(options) {
         self.globalOptions = options;
-        self.globalOptions.format = self.globalOptions.format || "json";
-
+        self.globalOptions.format = self.globalOptions.format || 'json';
         self.globalOptions.username = config.info.username;
         self.globalOptions.password = config.info.password;
         self.globalOptions.url = config.info.url;
     },
-    buildBaseUrlForActiveIteration : function(options) {
-
-        console.log('buildBaseUrlForActiveIteration');
-
-        var url = [];
-        url.push(config.info.url);
-        url.push("Assignables?take=50&format=");
-        url.push(config.info.format);             
-        url.push("&include=[Id,Description,Name,EntityType,Tags,Assignments[Id,Role,GeneralUser[FirstName,LastName,Email,Login]],Project[Name]]&where=");
-        url.push(encodeURIComponent("(EntityType.Name in ('UserStory','Bug'))"));
-        url.push(encodeURIComponent(" and (Iteration.EndDate eq '"));     
-        url.push(moment(options.date).format("YYYY-MM-DD"));           
-        url.push(encodeURIComponent("')"));
-        return url.join("");
+    itemParamMap: function(date) {
+        var map = {};
+        map['take'] = '50';
+        map['format'] = config.info.format;
+        map['include'] = '[Id,Description,Name,EntityType,Tags,Assignments[Id,Role,GeneralUser[FirstName,LastName,Email,Login]],Project[Name]]';
+        //the TP API requires "IN" items to be enclosed by single ticks!
+        map['where'] =  '(EntityType.Name in (\'UserStory\',\'Bug\'))'
+                        + ' and (Iteration.EndDate eq \'' + moment(date).format('YYYY-MM-DD') + '\')' ;
+        return map;
+    },
+    buildRequestParams: function(map, encode) {
+        var params = [], value;
+        for(var key in map) {
+            value = (encode) ? encodeURIComponent(map[key]) : map[key];
+            params.push(key + '=' + value);
+        }
+        return params.join('&');
     },
     getEntitiesForActiveIteration : function(callback, options) {
 
@@ -40,23 +42,35 @@ var methods = {
             username: config.info.username,
             password: config.info.password,
             url: options.url || config.info.url,
-            format: options.format || "json",
+            format: options.format || 'json',
             date: options.date || methods.globalOptions.date,
             parser: rest.parsers.json,
             iterationDurationInWeeks: config.info.iterationDurationInWeeks
         };
 
-        var baseUrl = methods.buildBaseUrlForActiveIteration(getOptions),
-            encodedEntityStateClause = encodeURIComponent(" and (EntityState.Name in ('To Verify','Done','Fixed','Closed'))"),
-            encodedTagsClause = encodeURIComponent(" and (Tags contains 'Demo')"),            
-            itemsUrl = baseUrl + encodedEntityStateClause,
-            taggedItemsUrl = baseUrl + encodedTagsClause;
+        var baseUrl = getOptions.url + 'Assignables?',
+            paramMap = methods.itemParamMap(getOptions.date),
+            entityStateClause = ' and (EntityState.Name in (\'To Verify\',\'Done\',\'Fixed\',\'Closed\'))';
+            encodedEntityStateClause = encodeURIComponent(entityStateClause),
+            tagsClause = '  and (Tags contains \'Demo\')';
+            encodedTagsClause = encodeURIComponent(tagsClause),
+            encodedParams = methods.buildRequestParams(paramMap, true),
+            plainParams = methods.buildRequestParams(paramMap, false),
+            itemsUrl = baseUrl + encodedParams + encodedEntityStateClause,
+            plainItemsUrl = baseUrl + plainParams + entityStateClause,
+            taggedItemsUrl = baseUrl + encodedParams +  encodedTagsClause,
+            plainTaggedItemsUrl = baseUrl + plainParams +  tagsClause;
 
-        //console.log("getEntitiesForActiveIteration 'normal items' TP API: \n" + itemsUrl);
+        //console.log('plain itemsUrl:  \n' + plainItemsUrl);
+        //console.log('encoded itemsUrl: \n' + itemsUrl);
+
         var itemResults;
         rest.get(itemsUrl,  getOptions)
             .on('success', function(data, response) {
                 itemResults = data; 
+
+                //console.log('plain taggedItemsUrl: \n' + plainTaggedItemsUrl);
+                //console.log('encoded taggedItemsUrl: \n' + taggedItemsUrl);
 
                 //we need the combined results...
                 rest.get(taggedItemsUrl, getOptions)
@@ -68,7 +82,7 @@ var methods = {
                             return item.Id;
                         });
 
-                        callback(itemResults);
+                    callback(itemResults);
 
                 }).on('fail', function(data, response) {
                     sys.puts('FAIL (get "tagged" items): \n' + data);
@@ -93,22 +107,19 @@ var methods = {
         });
     },
     getMostRecentIterationBoundary: function (callback, options) {
-
         console.log('getMostRecentIterationBoundary');
         
-        var url = [];
-        url.push(config.info.url);
-        url.push("Iterations?format=");
-        url.push(config.info.format);
-        url.push("&include=[EndDate]&where=");
-        url.push(encodeURIComponent("(EndDate lte '"));
-        url.push(moment().format("YYYY-MM-DD"));
-        url.push(encodeURIComponent("') and (EndDate gte '"));
-        url.push(moment().subtract("weeks", config.info.iterationDurationInWeeks).format("YYYY-MM-DD"));
-        url.push("')");
-        url = url.join("");
+        var paramMap = {};
+        paramMap['format'] = config.info.format;
+        paramMap['include'] = '[EndDate]';
+        paramMap['where'] = '(EndDate lte \'' + moment().format('YYYY-MM-DD') + '\')'
+                            + ' and (EndDate gte \'' + moment().subtract('weeks', config.info.iterationDurationInWeeks).format('YYYY-MM-DD') + '\')';
 
-        //logIt("getMostRecentIterationBoundary TP API: " + url);
+        var url = config.info.url + 'Iterations?' + methods.buildRequestParams(paramMap, true),
+            plainUrl = config.info.url + 'Iterations?' + methods.buildRequestParams(paramMap, false);
+
+        //console.log('plain URL: \n' + plainUrl);
+        //console.log('encoded URL: \n' + url);
 
         //handle the different events individually... 
         rest.get(url, config.info)
@@ -118,14 +129,14 @@ var methods = {
                     return new Date(parseInt(/\/Date\((\d+).*/.exec(item.EndDate)[1]));
                 });                                
                 var boundary = _.max(dates, function (date) { return date.getTime(); });
-                var formattedBoundary = moment(boundary).format("MM-DD-YYYY");
-                console.log("Boundary Date: " + formattedBoundary);
+                var formattedBoundary = moment(boundary).format('MM-DD-YYYY');
+                console.log('Boundary Date: ' + formattedBoundary);
                 callback(formattedBoundary);
  
         }).on('fail', function(data, response) {
-            sys.puts("FAIL: \n" + data);
+            sys.puts('FAIL: \n' + data);
         }).on('error', function(err, response) {
-            sys.puts("ERROR: " + err.message);
+            sys.puts('ERROR: ' + err.message);
             //TODO: figure out to decode the raw buffer, so we can know what happened!
             if (response) console.log(response.raw);
         }).on('complete', function(result, response) {
@@ -135,13 +146,12 @@ var methods = {
    }
 };
 api = function(methodName, callback, options) {
-    //logIt("Calling " + methodName);      
     if (methods[methodName]) {    
         return methods[methodName].apply(this, Array.prototype.splice.call(arguments, 1));
-    } else if (typeof methodName === "object" || !methodName) {
+    } else if (typeof methodName === 'object' || !methodName) {
         return methods.init.apply(this, arguments);
     } else {
-        throw  new Error("Method " + method + " does not exist in tp.api");
+        throw  new Error('Method ' + method + ' does not exist in tp.api');
     }
 }
 exports.api = api;
