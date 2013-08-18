294f2219ae2bcfc9910646baedd47600b6a8ea99
Rick Yoesting <rick.yoesting@gmail.com>
2013-08-18 17:53:22 -0500

    add example config files

---
+++ app.config.example.js
@@ -0,0 +1,7 @@
+app = {
+	title: "You-R-Here",
+	serverAddress: "http://localhost",
+	serverPort: 80,
+	plugin: "targetprocess"
+};
+exports.app = app;
---
+++ plugins/targetprocess.config.example.js
@@ -0,0 +1,10 @@
+info = {
+	username: "myusername",
+	password: "mysecretpassword",
+	url: "https://mycompany.tpondemand.com/api/v1/",
+	iterationDurationInWeeks: 2,
+	format: "json",
+	baseImageUrl: "https://mycompany.tpondemand.com",
+	orgName: "My Company"
+};
+exports.info = info;
